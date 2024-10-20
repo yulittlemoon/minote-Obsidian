e1309511a71d8f0e732e0de4b0deda156159e900
余芝君 <3047531663@qq.com>
2024-10-20 23:09:50 +0800

    添加同步图片功能

--- minote.py
+++ minote.py
@@ -1,8 +1,10 @@
 import requests
 import os
 import json
+import re
 from urllib.parse import unquote
 import argparse
+import shutil
 
 global save_dir
 # 小米云服务笔记 API URL
@@ -16,6 +18,21 @@ def get_notes(cookie):
         raise Exception('Failed to fetch data from API')
 
     return response.json().get('data', {}).get('entries', [])
+
+def get_redirected_url(url, headers=None):
+    try:
+        # 禁用代理和 SSL 验证
+        proxies = {
+            "http": None,
+            "https": None,
+        }
+        response = requests.get(url, headers=headers, allow_redirects=True, proxies=proxies, verify=False)
+        print(f"Final URL: {url}")
+        print(f"Redirected URL: {response.url}")
+        return response.url
+    except Exception as e:
+        print(f"Failed to get redirected URL for {url}: {e}")
+        return url  # 返回原始 URL 以继续处理
 
 def parse_extra_info(note):
     extra_info = note.get('extraInfo', '{}')
@@ -35,6 +52,7 @@ def get_note_content(note_id, cookie):
     title=title_json.get('title')
     content= response.json().get('data', {}).get('entry', {}).get('content', '')
     return title,content
+
 def sanitize_filename(title):
     # 替换或移除不合法字符
     safe_title = "".join([c if c.isalnum() or c in [' ', '_', '-'] else '' for c in title])
@@ -46,7 +64,7 @@ def create_unique_filename(title, note_id, save_dir,noteID):
     filename = base_filename
     # 检查文件是否已存在，如果是，则添加序号
     if os.path.exists(os.path.join(save_dir, filename)):
-        print(os.path.join(save_dir, filename))
+        # print(os.path.join(save_dir, filename))
         if note_id in noteID:
             filename = f"{title}.md"
         else:
@@ -54,7 +72,8 @@ def create_unique_filename(title, note_id, save_dir,noteID):
             counter += 1
     return filename
 
-def save_as_markdown(note, data, save_dir,noteId):
+def save_as_markdown(note, data, save_dir,noteId,cookie):
+    headers = {'Cookie': unquote(cookie)}
     note_id = note.get('id')
     title, content = data
     content = content.replace('<text indent="1">', '').replace('</text>', '').replace('\n', '\n').replace('<b>','**').replace('</b>','**')\
@@ -64,18 +83,30 @@ def save_as_markdown(note, data, save_dir,noteId):
         title = content[:30].strip()  # 从内容中提取前30个字符并去除前后空白
         title = title.split('\n', 1)[0]  # 只取第一行作为标题
     safe_title = sanitize_filename(title)
+    # 提取图片格式并替换
+    pattern = re.compile(r'(\d+\.[a-zA-Z0-9_-]+)<0/></>')
+    matches = pattern.findall(content)
+    for file_id in matches:
+        original_format = f"☺ {file_id}<0/></>"
+        image_url = f"https://i.mi.com/file/full?type=note_img&fileid={file_id}"
+        redirected_url = get_redirected_url(image_url, headers)
+        content = content.replace(original_format, f"![image]({redirected_url})")
 
     markdown_text = content
-    print(content)
+    # print(content)
     file_name = create_unique_filename(safe_title, note_id, save_dir,noteId)
     file_path = os.path.join(save_dir, file_name)
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(markdown_text)
 
+def clear_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory)
+
 def main():
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
-
     notes = get_notes(minote_cookie)
     for note in notes:
         note_id = note.get('id')
@@ -83,9 +114,11 @@ def main():
         with open('data.txt', 'w') as file:
             file.write(noteId_str)
         content = get_note_content(note_id, minote_cookie)
-        save_as_markdown(note, content, save_dir,noteId)
+        save_as_markdown(note, content, save_dir,noteId,minote_cookie)
         if note_id not in noteId:
             noteId.append(note_id)
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -93,12 +126,13 @@ if __name__ == "__main__":
     parser.add_argument("save_dir")
     options = parser.parse_args()
     minote_cookie = options.minote_cookie
-    save_dir=options.save_dir
+    save_dir = options.save_dir
+    clear_directory(save_dir)
     if os.path.exists('data.txt') and os.path.getsize('data.txt') > 0:
         with open('data.txt', 'r') as file:
             noteIdStr = file.read()
         noteId = noteIdStr.split(', ')
         noteId = list(map(int, noteId))
-        print(noteId)
+        # print(noteId)
     main()
 
